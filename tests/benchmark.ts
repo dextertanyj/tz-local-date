4a76dbb1330281e82595af14289a3433683a26f0
Dexter Tan <dev@dextertanyj.com>
2023-11-13 12:46:25 +0800

    test: benchmark and correctness

---
+++ tests/benchmark.ts
@@ -0,0 +1,88 @@
+import dayjs from "dayjs";
+import { Bench } from "tinybench";
+
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+import { LocalDate } from "~/index";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+dayjs.tz.setDefault("Asia/Singapore");
+
+const ld = new LocalDate("Asia/Singapore");
+let date1 = new Date();
+let date2 = new Date();
+
+const bench = new Bench({
+  time: 100,
+  setup: () => {
+    date1 = new Date();
+    date2 = new Date();
+  },
+});
+
+bench
+  .add("LocalDate:startOfDay", () => {
+    ld.startOfDay(date1);
+  })
+  .add("dayjs:startOfDay", () => {
+    dayjs(date1).tz().startOf("day");
+  })
+  .add("LocalDate:startOfNextDay", () => {
+    ld.startOfNextDay(date1);
+  })
+  .add("dayjs:startOfNextDay", () => {
+    dayjs(date1).tz().startOf("day").add(1, "day");
+  })
+  .add("LocalDate:startOfPreviousDay", () => {
+    ld.startOfPreviousDay(date1);
+  })
+  .add("dayjs:startOfPreviousDay", () => {
+    dayjs(date1).tz().startOf("day").subtract(1, "day");
+  })
+  .add("LocalDate:endOfDay", () => {
+    ld.endOfDay(date1);
+  })
+  .add("dayjs:endOfDay", () => {
+    dayjs(date1).tz().endOf("day");
+  })
+  .add("LocalDate:isSame", () => {
+    ld.isSame(date1, date2);
+  })
+  .add("dayjs:isSame", () => {
+    dayjs(date1).tz().isSame(date2, "day");
+  })
+  .add("LocalDate:isSameOrBefore", () => {
+    ld.isSameOrBefore(date1, date2);
+  })
+  .add("dayjs:isSameOrBefore", () => {
+    dayjs(date1).tz().isSameOrBefore(date2, "day");
+  })
+  .add("LocalDate:isSameOrAfter", () => {
+    ld.isSameOrAfter(date1, date2);
+  })
+  .add("dayjs:isSameOrAfter", () => {
+    dayjs(date1).tz().isSameOrAfter(date2, "day");
+  })
+  .add("LocalDate:isBefore", () => {
+    ld.isBefore(date1, date2);
+  })
+  .add("dayjs:isBefore", () => {
+    dayjs(date1).tz().isBefore(date2, "day");
+  })
+  .add("LocalDate:isAfter", () => {
+    ld.isAfter(date1, date2);
+  })
+  .add("dayjs:isAfter", () => {
+    dayjs(date1).tz().isAfter(date2, "day");
+  });
+
+await bench.run();
+
+console.table(bench.table());
---
+++ tests/correctness.spec.ts
@@ -0,0 +1,102 @@
+import dayjs from "dayjs";
+import { describe, expect, test } from "vitest";
+
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+import { fixtures } from "./correctness.fixtures";
+
+import { LocalDate } from "~/index";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+const timezones = ["Asia/Singapore", "UTC", "Pacific/Honolulu"];
+
+describe("consistency", () => {
+  for (const timezone of timezones) {
+    describe(timezone, () => {
+      const ld = new LocalDate(timezone);
+
+      test("startOfDay", () => {
+        for (const date of fixtures) {
+          expect(ld.startOfDay(date).valueOf()).toBe(
+            dayjs(date).tz(timezone).startOf("day").valueOf(),
+          );
+        }
+      });
+
+      test("startOfNextDay", () => {
+        for (const date of fixtures) {
+          expect(ld.startOfNextDay(date).valueOf()).toBe(
+            dayjs(date).tz(timezone).startOf("day").add(1, "day").valueOf(),
+          );
+        }
+      });
+
+      test("startOfPreviousDay", () => {
+        for (const date of fixtures) {
+          expect(ld.startOfPreviousDay(date).valueOf()).toBe(
+            dayjs(date).tz(timezone).startOf("day").subtract(1, "day").valueOf(),
+          );
+        }
+      });
+
+      test("endOfDay", () => {
+        for (const date of fixtures) {
+          expect(ld.endOfDay(date).valueOf()).toBe(dayjs(date).tz(timezone).endOf("day").valueOf());
+        }
+      });
+
+      test("isSame", () => {
+        for (const outer of fixtures) {
+          for (const inner of fixtures) {
+            expect(ld.isSame(outer, inner)).toBe(dayjs(outer).tz(timezone).isSame(inner, "day"));
+          }
+        }
+      });
+
+      test("isSameOrAfter", () => {
+        for (const outer of fixtures) {
+          for (const inner of fixtures) {
+            expect(ld.isSameOrAfter(outer, inner)).toBe(
+              dayjs(outer).tz(timezone).isSameOrAfter(inner, "day"),
+            );
+          }
+        }
+      });
+
+      test("isSameOrBefore", () => {
+        for (const outer of fixtures) {
+          for (const inner of fixtures) {
+            expect(ld.isSameOrBefore(outer, inner)).toBe(
+              dayjs(outer).tz(timezone).isSameOrBefore(inner, "day"),
+            );
+          }
+        }
+      });
+
+      test("isBefore", () => {
+        for (const outer of fixtures) {
+          for (const inner of fixtures) {
+            expect(ld.isBefore(outer, inner)).toBe(
+              dayjs(outer).tz(timezone).isBefore(inner, "day"),
+            );
+          }
+        }
+      });
+
+      test("isAfter", () => {
+        for (const outer of fixtures) {
+          for (const inner of fixtures) {
+            expect(ld.isAfter(outer, inner)).toBe(dayjs(outer).tz(timezone).isAfter(inner, "day"));
+          }
+        }
+      });
+    });
+  }
+});
